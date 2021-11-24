olivier-matz-6wind/dpdk

commit 6aebb942907da015be55338a912e83e370b2e102

kvargs: add function to get from key and value
A quite common scenario with kvargs is to lookup for a <key>=<value> in
a kvlist. For instance, check if name=foo is present in
name=toto,name=foo,name=bar. This is currently done in drivers/bus with
rte_kvargs_process() + the rte_kvargs_strcmp() handler.

This approach is not straightforward, and can be replaced by this new
function.

rte_kvargs_strcmp() is then removed.

Signed-off-by: Olivier Matz <olivier.matz@6wind.com>
Reviewed-by: Xueming Li <xuemingl@nvidia.com>
Reviewed-by: David Marchand <david.marchand@redhat.com>
Acked-by: Ray Kinsella <mdr@ashroe.eu>

drivers/bus/auxiliary/auxiliary_params.c
@@ -25,13 +25,12 @@ auxiliary_dev_match(const struct rte_device *dev,
 	      const void *_kvlist)
 {
 	const struct rte_kvargs *kvlist = _kvlist;
-	int ret;
+	const char *key = auxiliary_params_keys[RTE_AUXILIARY_PARAM_NAME];
 
-	ret = rte_kvargs_process(kvlist,
-			auxiliary_params_keys[RTE_AUXILIARY_PARAM_NAME],
-			rte_kvargs_strcmp, (void *)(uintptr_t)dev->name);
+	if (rte_kvargs_get_with_value(kvlist, key, dev->name) == NULL)
+		return -1;
 
-	return ret != 0 ? -1 : 0;
+	return 0;
 }
 
 void *

drivers/bus/vdev/vdev_params.c
@@ -26,19 +26,10 @@ static int
 vdev_dev_match(const struct rte_device *dev,
 	       const void *_kvlist)
 {
-	int ret;
 	const struct rte_kvargs *kvlist = _kvlist;
-	char *name;
+	const char *key = vdev_params_keys[RTE_VDEV_PARAM_NAME];
 
-	/* cannot pass const dev->name to rte_kvargs_process() */
-	name = strdup(dev->name);
-	if (name == NULL)
-		return -1;
-	ret = rte_kvargs_process(kvlist,
-		vdev_params_keys[RTE_VDEV_PARAM_NAME],
-		rte_kvargs_strcmp, name);
-	free(name);
-	if (ret != 0)
+	if (rte_kvargs_get_with_value(kvlist, key, dev->name) == NULL)
 		return -1;
 
 	return 0;

lib/kvargs/rte_kvargs.c
@@ -204,19 +204,32 @@ rte_kvargs_free(struct rte_kvargs *kvlist)
 	free(kvlist);
 }
 
+/* Lookup a value in an rte_kvargs list by its key and value. */
+const char *
+rte_kvargs_get_with_value(const struct rte_kvargs *kvlist, const char *key,
+			  const char *value)
+{
+	unsigned int i;
+
+	if (kvlist == NULL)
+		return NULL;
+	for (i = 0; i < kvlist->count; ++i) {
+		if (key != NULL && strcmp(kvlist->pairs[i].key, key) != 0)
+			continue;
+		if (value != NULL && strcmp(kvlist->pairs[i].value, value) != 0)
+			continue;
+		return kvlist->pairs[i].value;
+	}
+	return NULL;
+}
+
 /* Lookup a value in an rte_kvargs list by its key. */
 const char *
 rte_kvargs_get(const struct rte_kvargs *kvlist, const char *key)
 {
-	unsigned int i;
-
 	if (kvlist == NULL || key == NULL)
 		return NULL;
-	for (i = 0; i < kvlist->count; ++i) {
-		if (strcmp(kvlist->pairs[i].key, key) == 0)
-			return kvlist->pairs[i].value;
-	}
-	return NULL;
+	return rte_kvargs_get_with_value(kvlist, key, NULL);
 }
 
 /*
@@ -269,13 +282,4 @@ rte_kvargs_parse_delim(const char *args, const char * const valid_keys[],
 
 	free(copy);
 	return kvlist;
-}
-
-int
-rte_kvargs_strcmp(const char *key __rte_unused,
-		  const char *value, void *opaque)
-{
-	const char *str = opaque;
-
-	return -abs(strcmp(str, value));
 }

lib/kvargs/rte_kvargs.h
@@ -116,7 +116,7 @@ void rte_kvargs_free(struct rte_kvargs *kvlist);
 /**
  * Get the value associated with a given key.
  *
- * If multiple key matches, the value of the first one is returned.
+ * If multiple keys match, the value of the first one is returned.
  *
  * The memory returned is allocated as part of the rte_kvargs structure,
  * it must never be modified.
@@ -131,6 +131,33 @@ void rte_kvargs_free(struct rte_kvargs *kvlist);
  *   a value associated with a matching key otherwise.
  */
 const char *rte_kvargs_get(const struct rte_kvargs *kvlist, const char *key);
+
+/**
+ * @warning
+ * @b EXPERIMENTAL: this API may change without prior notice
+ *
+ * Get the value associated with a given key and value.
+ *
+ * Find the first entry in the kvlist whose key and value match the
+ * ones passed as argument.
+ *
+ * The memory returned is allocated as part of the rte_kvargs structure,
+ * it must never be modified.
+ *
+ * @param kvlist
+ *   A list of rte_kvargs pair of 'key=value'.
+ * @param key
+ *   The matching key. If NULL, any key will match.
+ * @param value
+ *   The matching value. If NULL, any value will match.
+ *
+ * @return
+ *   NULL if no key matches the input,
+ *   a value associated with a matching key otherwise.
+ */
+__rte_experimental
+const char *rte_kvargs_get_with_value(const struct rte_kvargs *kvlist,
+				      const char *key, const char *value);
 
 /**
  * Call a handler function for each key/value matching the key
@@ -169,32 +196,6 @@ int rte_kvargs_process(const struct rte_kvargs *kvlist,
  */
 unsigned rte_kvargs_count(const struct rte_kvargs *kvlist,
 	const char *key_match);
-
-/**
- * Generic kvarg handler for string comparison.
- *
- * This function can be used for a generic string comparison processing
- * on a list of kvargs.
- *
- * @param key
- *   kvarg pair key.
- *
- * @param value
- *   kvarg pair value.
- *
- * @param opaque
- *   Opaque pointer to a string.
- *
- * @return
- *   0 if the strings match.
- *   !0 otherwise or on error.
- *
- *   Unlike strcmp, comparison ordering is not kept.
- *   In order for rte_kvargs_process to stop processing on match error,
- *   a negative value is returned even if strcmp had returned a positive one.
- */
-__rte_experimental
-int rte_kvargs_strcmp(const char *key, const char *value, void *opaque);
 
 #ifdef __cplusplus
 }

lib/kvargs/version.map
@@ -14,6 +14,6 @@ DPDK_22 {
 EXPERIMENTAL {
 	global:
 
-	rte_kvargs_strcmp;
-
+	# added in 21.11
+	rte_kvargs_get_with_value;
 };

commit 0faa82961834edca708932cffb1346d6ff74a272

bus: fix device iterator match from arguments
Device iterator RTE_DEV_FOREACH() failed to return devices from
classifier like "class=vdpa", because matching name from empty kvargs
returns no result. If device name not specified in kvargs, the function
should iterate all devices.

This patch allows empty devargs or devargs without name specified.

Fixes: 6aebb942907d ("kvargs: add function to get from key and value")
Cc: olivier.matz@6wind.com

Signed-off-by: Xueming Li <xuemingl@nvidia.com>
Signed-off-by: Olivier Matz <olivier.matz@6wind.com>

drivers/bus/auxiliary/auxiliary_params.c
@@ -23,13 +23,19 @@ static const char * const auxiliary_params_keys[] = {
 };
 
 static int
-auxiliary_dev_match(const struct rte_device *dev,
-	      const void *_kvlist)
+auxiliary_dev_match(const struct rte_device *dev, const void *_kvlist)
 {
 	const struct rte_kvargs *kvlist = _kvlist;
 	const char *key = auxiliary_params_keys[RTE_AUXILIARY_PARAM_NAME];
+	const char *name;
 
-	if (rte_kvargs_get_with_value(kvlist, key, dev->name) == NULL)
+	/* no kvlist arg, all devices match */
+	if (kvlist == NULL)
+		return 0;
+
+	/* if key is present in kvlist and does not match, filter device */
+	name = rte_kvargs_get(kvlist, key);
+	if (name != NULL && strcmp(name, dev->name))
 		return -1;
 
 	return 0;

drivers/bus/vdev/vdev_params.c
@@ -28,8 +28,15 @@ vdev_dev_match(const struct rte_device *dev,
 {
 	const struct rte_kvargs *kvlist = _kvlist;
 	const char *key = vdev_params_keys[RTE_VDEV_PARAM_NAME];
+	const char *name;
 
-	if (rte_kvargs_get_with_value(kvlist, key, dev->name) == NULL)
+	/* no kvlist arg, all devices match */
+	if (kvlist == NULL)
+		return 0;
+
+	/* if key is present in kvlist and does not match, filter device */
+	name = rte_kvargs_get(kvlist, key);
+	if (name != NULL && strcmp(name, dev->name))
 		return -1;
 
 	return 0;

app/test/test_devargs.c
@@ -12,6 +12,8 @@
 #include <rte_bus.h>
 #include <rte_class.h>
 
+#include <rte_bus_vdev.h>
+
 #include "test.h"
 
 /* Check layer arguments. */
@@ -202,6 +204,151 @@ test_invalid_devargs(void)
 }
 
 static int
+cmp_dev_name(const struct rte_device *dev, const void *name)
+{
+	return strcmp(dev->name, name);
+}
+
+#define TEST_DEVARGS_KEY_NAME "name"
+
+static const char * const valid_keys[] = {
+	TEST_DEVARGS_KEY_NAME,
+	NULL,
+};
+
+static int
+cmp_dev_match(const struct rte_device *dev, const void *_kvlist)
+{
+	const struct rte_kvargs *kvlist = _kvlist;
+	const char *key = TEST_DEVARGS_KEY_NAME;
+	const char *name;
+
+	/* no kvlist arg, all devices match */
+	if (kvlist == NULL)
+		return 0;
+
+	/* if key is present in kvlist and does not match, filter device */
+	name = rte_kvargs_get(kvlist, key);
+	if (name != NULL && strcmp(name, dev->name))
+		return -1;
+
+	return 0;
+}
+
+/* get vdev matching kvargs */
+static struct rte_device *
+get_matching_vdev(const char *match_str)
+{
+	struct rte_bus *vdev_bus = rte_bus_find_by_name("vdev");
+	struct rte_kvargs *kvargs = NULL;
+	struct rte_device *dev;
+
+	if (match_str != NULL) {
+		kvargs = rte_kvargs_parse(match_str, valid_keys);
+		if (kvargs == NULL) {
+			printf("Failed to parse match string\n");
+			return NULL;
+		}
+	}
+
+	dev = vdev_bus->find_device(NULL, cmp_dev_match, kvargs);
+	rte_kvargs_free(kvargs);
+
+	return dev;
+}
+
+static int
+test_vdev_devargs(void)
+{
+	struct rte_bus *vdev_bus = rte_bus_find_by_name("vdev");
+	struct rte_dev_iterator dev_iter = { 0 };
+	struct rte_device *dev, *dev0, *dev1;
+
+	/* not supported */
+	if (vdev_bus == NULL)
+		return 0;
+
+	/* create first vdev */
+	if (rte_vdev_init("net_null_test0", "") < 0) {
+		printf("Failed to create vdev net_null_test0\n");
+		goto fail;
+	}
+	dev0 = vdev_bus->find_device(NULL, cmp_dev_name, "net_null_test0");
+	if (dev0 == NULL) {
+		printf("Cannot find net_null_test0 vdev\n");
+		goto fail;
+	}
+
+	/* create second vdev */
+	if (rte_vdev_init("net_null_test1", "") < 0) {
+		printf("Failed to create vdev net_null_test1\n");
+		goto fail;
+	}
+	dev1 = vdev_bus->find_device(NULL, cmp_dev_name, "net_null_test1");
+	if (dev1 == NULL) {
+		printf("Cannot find net_null_test1 vdev\n");
+		goto fail;
+	}
+
+	/* try to match vdevs */
+	dev = get_matching_vdev("name=net_null_test0");
+	if (dev != dev0) {
+		printf("Cannot match net_null_test0 vdev\n");
+		goto fail;
+	}
+
+	dev = get_matching_vdev("name=net_null_test1");
+	if (dev != dev1) {
+		printf("Cannot match net_null_test1 vdev\n");
+		goto fail;
+	}
+
+	dev = get_matching_vdev("name=unexistant");
+	if (dev != NULL) {
+		printf("Unexistant vdev should not match\n");
+		goto fail;
+	}
+
+	dev = get_matching_vdev("");
+	if (dev == NULL || dev == dev1) {
+		printf("Cannot match any vdev with empty match string\n");
+		goto fail;
+	}
+
+	dev = get_matching_vdev(NULL);
+	if (dev == NULL || dev == dev1) {
+		printf("Cannot match any vdev with NULL match string\n");
+		goto fail;
+	}
+
+	/* iterate all vdevs, and ensure we find vdev0 and vdev1 */
+	RTE_DEV_FOREACH(dev, "bus=vdev", &dev_iter) {
+		if (dev == dev0)
+			dev0 = NULL;
+		else if (dev == dev1)
+			dev1 = NULL;
+	}
+	if (dev0 != NULL) {
+		 printf("dev0 was not iterated\n");
+		 goto fail;
+	}
+	if (dev1 != NULL) {
+		 printf("dev1 was not iterated\n");
+		 goto fail;
+	}
+
+	rte_vdev_uninit("net_null_test0");
+	rte_vdev_uninit("net_null_test1");
+
+	return 0;
+
+fail:
+	rte_vdev_uninit("net_null_test0");
+	rte_vdev_uninit("net_null_test1");
+	return -1;
+}
+
+static int
 test_devargs(void)
 {
 	printf("== test valid case ==\n");
@@ -209,6 +356,9 @@ test_devargs(void)
 		return -1;
 	printf("== test invalid case ==\n");
 	if (test_invalid_devargs() < 0)
+		return -1;
+	printf("== test vdev args ==\n");
+	if (test_vdev_devargs() < 0)
 		return -1;
 	return 0;
 }